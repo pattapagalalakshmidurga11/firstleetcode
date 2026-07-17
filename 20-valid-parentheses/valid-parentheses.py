6d1a86e71206bc1c6a193f1887213fd30460f884
pattapagalalakshmidurga11 <pattapagalalakshmidurga11@gmail.com>
2026-07-17 19:30:13 +0530

    Time: 1 ms (45.22%) | Memory: 19.2 MB (91.84%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        for c in s:
+            if c=="(":
+                stack.append(")")
+            elif c=="[":
+                stack.append("]")
+            elif c=="{":
+                stack.append("}")
+            elif not stack or stack.pop()!=c:
+                return False
+        if len(stack)==0:
+            return True
+        else:
+            return False
+
+        
