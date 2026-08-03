48f0a151130ec58ab3dfbe43f6d4da28f65fdf9c
pattapagalalakshmidurga11 <pattapagalalakshmidurga11@gmail.com>
2026-08-03 13:11:06 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (17.08%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        d=[0]*(n+1)
+        d[0]=1
+        d[1]=1
+        for i in range(2,n+1):
+            d[i]=d[i-1]+d[i-2]
+        return d[-1]
+        
